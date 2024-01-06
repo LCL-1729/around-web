ad9e3dca30b46139793551588ccb547e321b8807
Felix Qin <felixqin@Felixs-MBP.lan>
2024-01-05 21:37:06 -0800

    Committer: Felix Qin <felixqin@Felixs-MBP.lan>
     On branch main
     Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	deleted:    src/App.js
    	new file:   src/components/App.js
    	new file:   src/components/Main.js
    	new file:   src/components/TopBar.js
    	modified:   src/index.css
    	renamed:    src/App.css -> src/styles/App.css
    	new file:   src/styles/TopBar.css
    	renamed:    src/App.test.js -> src/tests/App.test.js

---
+++ src/components/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import TopBar from "./TopBar";
+
+import { TOKEN_KEY } from "../constants";
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem(TOKEN_KEY) ? true : false
+  );
+
+  const logout = () => {
+    console.log("log out");
+    localStorage.removeItem(TOKEN_KEY);
+    setIsLoggedIn(false);
+  };
+
+  const loggedIn = (token) => {
+    if (token) {
+      localStorage.setItem(TOKEN_KEY, token);
+      setIsLoggedIn(true);
+    }
+  };
+  return (
+    <div className="App">
+      <TopBar isLoggedIn={isLoggedIn} handleLogout={logout} />
+    </div>
+  );
+}
+
+export default App;
