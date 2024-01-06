f57d8f8bf6e026eb2fc9c65ec90dc9d9cc122ae6
Felix Qin <felixqin@Felixs-MBP.lan>
2024-01-06 01:08:08 -0800

    new file:   src/components/SearchBar.js 	modified:   src/constants.js 	modified:   src/index.css 	new file:   src/styles/SearchBar.css

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const TOKEN_KEY = 'token';
 export const BASE_URL = 'https://superb-acumen-400518.uc.r.appspot.com';
+export const SEARCH_KEY = {
+    all: 0,
+    keyword: 1,
+    user: 2
+}
